2ec61d596a8313e1356efad4dda36b4cdf3e7fa6
arora <aroramadhav65@gmail.com>
2022-05-10 20:17:34 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,6 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           // to see the wireframe for each widget.
           //
-          // Column has various properties to control how it sizes itself and
           // how it positions its children. Here we use mainAxisAlignment to
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
