07b8f75ac89a73277b590522abdecc2b1c974a53
arora <aroramadhav65@gmail.com>
2022-05-10 20:17:00 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -43,11 +43,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
           // Column is also a layout widget. It takes a list of children and
