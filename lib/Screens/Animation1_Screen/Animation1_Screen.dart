23fc4f6a661a5dc2cf6135c2c41d203a0fdf0a8b
arora <aroramadhav65@gmail.com>
2022-05-11 19:33:41 +0530

    Update

--- lib/Screens/Animation1_Screen/Animation1_Screen.dart
+++ lib/Screens/Animation1_Screen/Animation1_Screen.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+class AnimationScreen extends StatefulWidget {
+  const AnimationScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<AnimationScreen> createState() => _AnimationScreenState();
+}
+
+class _AnimationScreenState extends State<AnimationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
