9c9655032bd4fd7fc83c36c8b9eb3c8e4da8a41d
arora <aroramadhav65@gmail.com>
2022-05-12 19:48:50 +0530

    Update

--- lib/Screens/Animation2_Screen/Animation2_Screen.dart
+++ lib/Screens/Animation2_Screen/Animation2_Screen.dart
@@ -12,6 +12,12 @@ class Animation2Screen extends StatefulWidget {
 class _Animation2ScreenState extends State<Animation2Screen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Lottie.network(
+        'https://assets3.lottiefiles.com/packages/lf20_jrpzvtqz.json',
+        width: MediaQuery.of(context).size.width / 2,
+        height: MediaQuery.of(context).size.height / 2,
+      ),
+    );
   }
 }
