7dc996e7a7681ae04dc1bfebdbe69c1f2e0292fb
arora <aroramadhav65@gmail.com>
2022-05-12 19:58:55 +0530

    Update

--- lib/Screens/Animation2_Screen/Animation2_Screen.dart
+++ lib/Screens/Animation2_Screen/Animation2_Screen.dart
@@ -19,6 +19,7 @@ class _Animation2ScreenState extends State<Animation2Screen>
     super.initState();
 
     _controller = AnimationController(
+      duration: Duration(seconds: 2),
       vsync: this,
     );
   }
