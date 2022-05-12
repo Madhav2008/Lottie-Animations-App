addbd81da535122e86056e8fcf3de7cd72e70753
arora <aroramadhav65@gmail.com>
2022-05-12 22:54:07 +0530

    Update

--- lib/Screens/Navigation_Screen/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen/Navigation_Screen.dart
@@ -46,9 +46,9 @@ class _NavigationScreenState extends State<NavigationScreen> {
             color: (_page == 2) ? const Color(0xFF2a46c2) : Colors.white,
           ),
         ],
-        color: const Color(0xFF3e3ffb),
-        buttonBackgroundColor: Colors.grey[50],
-        backgroundColor: const Color(0xFF553fff),
+        // color: const Color(0xFF3e3ffb),
+        // buttonBackgroundColor: Colors.grey[50],
+        // backgroundColor: const Color(0xFF553fff),
         animationDuration: const Duration(milliseconds: 500),
         onTap: (index) {
           setState(() {
