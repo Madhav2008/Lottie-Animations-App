a445ec9a63014fd0d8fdc9f5cc2459950adb0250
arora <aroramadhav65@gmail.com>
2022-05-12 19:12:36 +0530

    Update

--- lib/Screens/Navigation_Screen/Navigation_Screen.dart
+++ lib/Screens/Navigation_Screen/Navigation_Screen.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: file_names
 
-import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:lottie_animations_app/Screens/Animation1_Screen/Animation1_Screen.dart';
