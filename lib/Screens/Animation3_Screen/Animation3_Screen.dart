ca364d8edd01b956193392b2722a6d8bc45a1127
arora <aroramadhav65@gmail.com>
2022-05-12 22:59:20 +0530

    Update

--- lib/Screens/Animation3_Screen/Animation3_Screen.dart
+++ lib/Screens/Animation3_Screen/Animation3_Screen.dart
@@ -13,11 +13,13 @@ class Animation3Screen extends StatefulWidget {
 class _Animation3ScreenState extends State<Animation3Screen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Lottie.network(
-        'https://assets6.lottiefiles.com/private_files/lf30_ll1hdda1.json',
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
+    return Scaffold(
+      child: Center(
+        child: Lottie.network(
+          'https://assets6.lottiefiles.com/private_files/lf30_ll1hdda1.json',
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+        ),
       ),
     );
   }
