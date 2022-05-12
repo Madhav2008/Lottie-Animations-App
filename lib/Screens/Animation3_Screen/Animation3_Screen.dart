581eab93ec98b81744d73d0222b3a296842ad1cf
arora <aroramadhav65@gmail.com>
2022-05-12 22:59:25 +0530

    Update

--- lib/Screens/Animation3_Screen/Animation3_Screen.dart
+++ lib/Screens/Animation3_Screen/Animation3_Screen.dart
@@ -14,7 +14,7 @@ class _Animation3ScreenState extends State<Animation3Screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      child: Center(
+      body: Center(
         child: Lottie.network(
           'https://assets6.lottiefiles.com/private_files/lf30_ll1hdda1.json',
           width: MediaQuery.of(context).size.width,
