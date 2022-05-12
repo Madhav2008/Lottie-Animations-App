34bcc184ea993e03ff8fd22478a7611303bd735c
arora <aroramadhav65@gmail.com>
2022-05-12 22:39:05 +0530

    Update

--- lib/Screens/Animation3_Screen/Animation3_Screen.dart
+++ lib/Screens/Animation3_Screen/Animation3_Screen.dart
@@ -17,6 +17,7 @@ class _Animation3ScreenState extends State<Animation3Screen> {
       child: Lottie.network(
         'https://assets6.lottiefiles.com/private_files/lf30_ll1hdda1.json',
         width: MediaQuery.of(context).size.width,
+        width: MediaQuery.of(context).size.width,
       ),
     );
   }
