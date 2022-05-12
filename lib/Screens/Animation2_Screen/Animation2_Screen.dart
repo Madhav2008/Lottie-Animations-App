a015b8d865b859a3267fb98d1fa3735bc97e7e5b
arora <aroramadhav65@gmail.com>
2022-05-12 19:54:24 +0530

    Update

--- lib/Screens/Animation2_Screen/Animation2_Screen.dart
+++ lib/Screens/Animation2_Screen/Animation2_Screen.dart
@@ -22,6 +22,10 @@ class _Animation2ScreenState extends State<Animation2Screen>
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
