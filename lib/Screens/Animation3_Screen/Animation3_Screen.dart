a7682f50abfebceafe6adff8cdf44aa4e9fb6b36
arora <aroramadhav65@gmail.com>
2022-05-12 22:38:16 +0530

    Update

--- lib/Screens/Animation3_Screen/Animation3_Screen.dart
+++ lib/Screens/Animation3_Screen/Animation3_Screen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class Animation3Screen extends StatefulWidget {
   const Animation3Screen({Key? key}) : super(key: key);
@@ -12,6 +13,8 @@ class Animation3Screen extends StatefulWidget {
 class _Animation3ScreenState extends State<Animation3Screen> {
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return Center(
+      child: Lottie.network(url),
+    );
   }
 }
