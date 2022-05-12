8dd6e7b6152d837ee61eb1726bd8ad4caa87af07
arora <aroramadhav65@gmail.com>
2022-05-12 23:04:35 +0530

    Update

--- lib/Screens/Animation2_Screen/Animation2_Screen.dart
+++ lib/Screens/Animation2_Screen/Animation2_Screen.dart
@@ -53,7 +53,7 @@ class _Animation2ScreenState extends State<Animation2Screen>
       body: Center(
         child: Column(
           children: [
-            Text(''),
+            const Text(''),
             const SizedBox(
               height: 20,
             ),
